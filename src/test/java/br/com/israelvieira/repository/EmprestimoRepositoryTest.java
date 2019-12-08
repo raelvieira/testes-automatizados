8b4aaa61b37fc5ec58548a9a61680bd1e25ee66d
Israel F. Vieira <israelv.generation@gmail.com>
2019-12-08 17:51:22 -0300

    Implementação dos testes do Repositório de Emprestimo

--- src/test/java/br/com/israelvieira/repository/EmprestimoRepositoryTest.java
+++ src/test/java/br/com/israelvieira/repository/EmprestimoRepositoryTest.java
@@ -1,25 +1,36 @@
 package br.com.israelvieira.repository;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import br.com.israelvieira.builder.EmprestimoBuilder;
+import br.com.israelvieira.builder.LivroBuilder;
+import br.com.israelvieira.modelo.Emprestimo;
+import br.com.israelvieira.modelo.Livro;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.util.List;
 
 public class EmprestimoRepositoryTest {
 
     private EntityManager manager;
     private static EntityManagerFactory emf;
-    private EmprestimoRepository repositorio;
+    private EmprestimoRepository repositorioEmprestimo;
+    private LivroRepository repositorioLivro;
+
+    @BeforeAll
+    public static void inicio() {
+        emf = Persistence.createEntityManagerFactory("emprestimos");
+    }
 
     @BeforeEach
     public void antes() {
         manager = emf.createEntityManager();
         manager.getTransaction().begin();
 
-        repositorio = new EmprestimoRepository(manager);
+        repositorioEmprestimo = new EmprestimoRepository(manager);
+        repositorioLivro = new LivroRepository(manager);
     }
 
     @AfterEach
@@ -33,7 +44,38 @@ public class EmprestimoRepositoryTest {
     }
 
     @Test
-    public void deveSalvarUmNovoEmprestimo(){
+    public void deveTrazerListaComLivrosEmprestados(){
+        Livro livro1 = LivroBuilder.umLivro().emprestado().constroi();
+        Livro livro2 = LivroBuilder.umLivro().emprestado().comTitulo("Tubarão").constroi();
 
+        Emprestimo emprestimo1 = EmprestimoBuilder.umEmprestimo().comLivro(livro1).constroi();
+        Emprestimo emprestimo2 = EmprestimoBuilder.umEmprestimo().comLivro(livro2).constroi();
+
+        repositorioLivro.salva(livro1);
+        repositorioLivro.salva(livro2);
+        repositorioEmprestimo.salva(emprestimo1);
+        repositorioEmprestimo.salva(emprestimo2);
+
+        List<Emprestimo> listaEmprestimosNaValidade = repositorioEmprestimo.livrosEmEmprestimo();
+
+        Assertions.assertEquals(2, listaEmprestimosNaValidade.size() );
+    }
+
+    @Test
+    public void deveTrazerListaComLivrosAtrasados(){
+        Livro livro1 = LivroBuilder.umLivro().comTitulo("O Empréstimo").constroi();
+        Livro livro2 = LivroBuilder.umLivro().comTitulo("O Procurado").constroi();
+
+        Emprestimo emprestimo1 = EmprestimoBuilder.umEmprestimo().comDataRetorno(LocalDate.now().minusDays(1)).comLivro(livro1).constroi();
+        Emprestimo emprestimo2 = EmprestimoBuilder.umEmprestimo().comDataRetorno(LocalDate.now().minusDays(2)).comLivro(livro2).constroi();
+
+        repositorioLivro.salva(livro1);
+        repositorioLivro.salva(livro2);
+        repositorioEmprestimo.salva(emprestimo1);
+        repositorioEmprestimo.salva(emprestimo2);
+
+        List<Emprestimo> listaEmprestimosComLivrosAtrasados = repositorioEmprestimo.atrasadosParaDevolucao();
+
+        Assertions.assertEquals(2, listaEmprestimosComLivrosAtrasados.size() );
     }
 }
